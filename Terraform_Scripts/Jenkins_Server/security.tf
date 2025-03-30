6cce0781d63f3a7c790a06e570a2416129246233
hodaifa-ech <150153831+hodaifa-ech@users.noreply.github.com>
2025-03-30 16:59:07 +0000

    add app

--- Terraform_Scripts/Jenkins_Server/security.tf
+++ Terraform_Scripts/Jenkins_Server/security.tf
@@ -20,8 +20,19 @@ resource "aws_security_group" "web-traffic" {
       to_port=port.value
       protocol= "TCP"
       cidr_blocks =["0.0.0.0/0"]
+    } 
+     }
+  dynamic "ingress" {
+    iterator = port
+    for_each = var.ingressrules2
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "TCP"
+      cidr_blocks = ["0.0.0.0/0"]
     }
   }
+
   tags = {
     "Name" = "My_SG1"
   }
